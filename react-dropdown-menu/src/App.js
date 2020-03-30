353a2c9c3d41b2da75644bee8dcbf70229718a2e
Mimi <mimitraugott@gmail.com>
2020-03-29 18:59:35 -0600

    dropdownmenu

---
+++ react-dropdown-menu/src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import MenuBar from './MenuBar'
+
+class App extends Component {
+  render(){
+  return (
+    <div>
+      <MenuBar/>
+    </div>
+  );
+}
+}
+
+export default App;
